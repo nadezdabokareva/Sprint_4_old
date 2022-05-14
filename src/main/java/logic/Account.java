99aa4484f42347bc9e9487a8273e1c9aa22e3d45
Надежда Сергеевна Бокарева <87896632+nadezdabokareva@users.noreply.github.com>
2022-05-14 19:45:20 +0300

    Revert "second commit"

--- src/main/java/logic/Account.java
+++ src/main/java/logic/Account.java
@@ -2,49 +2,17 @@ package logic;
 
 public class Account {
 
-    public final String name;
+    private final String name;
 
     public Account(String name) {
         this.name = name;
     }
 
     public boolean checkNameToEmboss() {
-        if (this.name.length() >= 3
-                && this.name.length() <= 19
-                && checkOneChar(this.name, ' ')
-                && !this.name.startsWith(" ")
-                && !this.name.endsWith(" ")) {
-            return true;
-        }
-        return false;
+        /*
+             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
+             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
+         */
     }
 
-    private boolean checkOneChar(String string, char symbol) {
-        int count = 0;
-        for (char litera : string.toCharArray()) {
-            if (litera == symbol) {
-                if (count >= 1) return false;
-                count++;
-            }
-        }
-        return count == 1;
-    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
